be99fb25d7cdd3390911ba10cd669c5d421f4783
Zain Khan <zainkhan@Zains-MBP.home>
2020-08-03 00:03:33 -0700

    Updates

---
+++ precache-manifest.807ad6e0134e3066cd044047f0203ae3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3eb34acd6a9525d66f8c855299bd688f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ee7d04d2ef9449ad69f7",
+    "url": "/static/css/main.f0e6ae81.chunk.css"
+  },
+  {
+    "revision": "c0b9582d0c2a95dc6983",
+    "url": "/static/js/2.f14cea11.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f14cea11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee7d04d2ef9449ad69f7",
+    "url": "/static/js/main.9668eb50.chunk.js"
+  },
+  {
+    "revision": "a0003e65cf28c585feb8",
+    "url": "/static/js/runtime-main.6ecb8fcc.js"
+  }
+]);
